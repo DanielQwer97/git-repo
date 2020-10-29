2d0d32ea192fae1a391b4c048bbe8f3bd827aa67
DanielQwer97 <Akahs16@gmail.com>
2020-10-29 15:09:12 -0700

    VUE-02, Componentes y props

--- VUE-02/mi-proyecto/src/App.vue
+++ VUE-02/mi-proyecto/src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="titulo" fecha="2020-10-28" />
+    <ListaElementos titulo="Mi inventario de frutas" :elementos="frutas" />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ListaElementos  from './components/ListaElementos.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ListaElementos
+  },
+  data(){
+    return {
+      titulo: 'Mi titulo en vue',
+      frutas: [
+        { id: 1, nombre: 'Manzana', cantidad: 10 },
+        { id: 2, nombre: 'Uva', cantidad: 5 },
+        { id: 3, nombre: 'Fresa', cantidad: 7 }
+      ]
+    }
   }
 }
 </script>
@@ -24,3 +39,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
---
+++ VUE-02/mi-proyecto/src/components/AccionElemento.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="accion">{{texto}}</button>
+</template>
+
+<script>
+export default {
+    name: 'AccionElemento',
+    props: {
+        texto: String
+    }, 
+    methods: {
+        accion(){
+            this.$emit('accion');
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+button {
+    margin-left: 15px;
+}
+</style>
