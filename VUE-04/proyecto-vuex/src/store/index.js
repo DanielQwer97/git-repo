95bd4a2cf1df04a411be3983cb23fa9c9f98dde8
DanielQwer97 <Akahs16@gmail.com>
2020-11-17 14:24:11 -0700

    Proyecto-vuex

---
+++ VUE-04/proyecto-vuex/src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    total: 90,
+    otra: 'hola'
+  },
+  mutations: {
+    aumentar(state, num) {
+      state.total = state.total + num;
+    },
+    disminuir(state) {
+      state.total--;
+    }
+  },
+  actions: {
+    accionAccion({commit}, num){
+      commit('aumentar', num);
+    },
+    accionDisminuir({commit}){
+      commit('disminuir');
+    }
+  },
+  modules: {
+  }
+})
---
+++ VUE-04/proyecto-vuex/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" width="30">
+    <h1>{{titulo}}</h1>
+    <p :style="colorTotal">Mi total: {{ total }}</p>
+    <button @click="accionAccion(5)">Aumentar</button>
+    <p> {{ $store.state.otra }}</p>
+    <BotonDisminuir />
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+import BotonDisminuir from '../components/BotonDisminuir'
+
+export default {
+  name: 'Home',
+  components: {
+    BotonDisminuir
+  },
+  data() {
+    return {
+      titulo: 'Mi proyecto con vuex'
+    }
+  },
+  methods: {
+    ...mapActions(['accionAccion'])
+  },
+  computed: {
+    ...mapState(['total']),
+    colorTotal() {
+      return this.total >= 100 ? {'color': 'green'} : {'color': 'red'};
+    }
+  }
+}
+</script>
