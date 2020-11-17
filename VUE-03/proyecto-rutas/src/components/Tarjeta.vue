30d89c87aec1efe6423fbcc793a8615a77773c82
DanielQwer97 <Akahs16@gmail.com>
2020-11-17 14:19:16 -0700

    Proyecto-rutas

---
+++ VUE-03/proyecto-rutas/src/components/Tarjeta.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="alert alert-primary" role="alert">
+      <router-link :to="`/peliculas/${id}`">
+         {{titulo}}
+     </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Tarjeta',
+    props:{
+        titulo: String,
+        id: String
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ VUE-03/proyecto-rutas/src/views/DetallePelicula.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>Detalle de Película</div>
+  <div>{{$route.params.idx}}</div>
+  <p>{{pelicula.description}}</p>
+</template>
+
+<script>
+
+export default {
+    name: 'DetallePelicula',
+    data() {
+        return {
+            pelicula: {}
+        }
+    },
+    methods: {
+        async consumirPelicula() {
+            try {
+               const res = await fetch(`https://ghibliapi.herokuapp.com/films/${this.$route.params.idx}`);
+                const data = await res.json();
+                console.log(data);
+                this.pelicula = data;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    },
+    created() {
+        this.consumirPelicula();
+    }
+
+}
+</script>
+
+<style>
+
+</style>
