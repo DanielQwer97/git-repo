7f6057a69a271d34ee591eb59d0ae0bfd785145a
DanielQwer97 <Akahs16@gmail.com>
2020-11-20 14:38:12 -0700

    VUE-07 Conexion, Agregar Personas

---
+++ Vue-07/mi-proyecto/src/views/Agregar.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <h1>Agregar Personas</h1>
+        <b-form @submit.prevent="guardarPersona">
+            <Input 
+              v-model="persona.nombre"
+              label="Nombre"
+              placeholder="Ingrese el nombre de la persona"
+              id="nombre"
+            />
+            <Input 
+              v-model="persona.direccion"
+              label="Dirección"
+              placeholder="Ingrese la dirección de la persona"
+              :maxlength="150"
+              id="direccion"
+            />
+            <Input 
+              v-model="persona.telefono"
+              label="Teléfono"
+              placeholder="Ingrese el teléfono de la persona"
+              :maxlength="10"
+              id="telefono"
+            />
+            <b-button variant="primary" type="submit" class="float-right mt-3">
+            Guardar
+            </b-button>
+        </b-form>
+    </div>
+</template>
+
+<script>
+import Input from '../components/input'
+import { mapActions } from 'vuex'
+    export default {
+       name:'Agregar',
+       components:{
+           Input
+       },
+       data(){
+           return{
+               persona:{
+                   nombre:'',
+                   direccion:'',
+                   telefono:''
+               }
+           }
+       },
+       methods:{
+           ...mapActions(['crearPersona']),
+           guardarPersona(){
+              this.crearPersona({
+                  params: this.persona,
+                  onComplete: (response) => {
+                      console.log(response)
+                      this.$router.push({
+                          name: 'Home'
+                      })
+                  }
+              })
+           }
+       }
+        
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Vue-07/mi-proyecto/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+       <h1>Personas</h1>
+       <b-button to="/Agregar-Persona" variant="primary" class="float-right mb-3">Agregar</b-button>
+       <Table :campos="campos" :items="allPersonas"></Table>
+  </div>
+</template>
+
+<script>
+
+import Table from "../components/table"
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'Home',
+  components: {
+     Table
+  },
+   data() {
+    return {
+      campos: [
+        { key: 'id', label: 'Clave' },
+        { key: 'nombre', label: 'Nombre'},
+        { key: 'direccion', label: 'Dirección'},
+        { key: 'telefono', label: 'Teléfono'}
+      ]
+    }  
+  },
+  computed: {
+    ...mapGetters(['allPersonas'])
+  },
+  methods: {
+    ...mapActions(['setPersonas'])
+  },
+  created() {
+    this.setPersonas();
+  }
+}
+</script>
